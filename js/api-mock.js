20548bdc94ea87e8183aedca6e6c988245ea00d2
Minega Shyaka Patrick <minega25@gmail.com>
2020-05-07 11:17:24 +0200

    create base folder structure and api mock data

---
+++ js/api-mock.js
@@ -0,0 +1,21 @@
+export default [
+    {
+        id: 1,
+        name: 'Bronze',
+        price: 49,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et '
+    },
+    {
+        id: 2,
+        name: 'Silver',
+        price: 69,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et '
+    },
+    {
+        id: 3,
+        name: 'Gold',
+        price: 99,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et '
+    }
+];
+
